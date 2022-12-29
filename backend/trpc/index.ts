b41a0f542459870ab0a5de3753b650f3d1a298f5
Snehanshu <hello@snehanshu.com>
2022-12-29 14:42:39 +0530

    organize trpc

--- backend/trpc/index.ts
+++ backend/trpc/index.ts
@@ -1,8 +1,16 @@
-import { initTRPC } from "@trpc/server";
-import type { Context } from "./context";
+import prisma from "../prisma";
+import { publicProcedure, router } from "./trpc";
 
-const t = initTRPC.context<Context>().create();
+export const appRouter = router({
+  health: publicProcedure.query(async ({ input, ctx }) => {
+    // Test DB connection
+    const result = await prisma.$queryRaw`select 1+1 sum`;
 
-export const appRouter = t.router({});
+    return {
+      time: new Date(), // Current server time
+      db: result,
+    };
+  }),
+});
 
 export type AppRouter = typeof appRouter;
