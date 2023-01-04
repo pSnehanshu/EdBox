c30953760b40515eaa19a68c681b9159eeae4198
Snehanshu <hello@snehanshu.com>
2023-01-04 14:16:28 +0530

    util functions for sqlite

--- app/utils/db.tsx
+++ app/utils/db.tsx
@@ -34,3 +34,47 @@ export function useDB() {
   const db = useContext(dbContext);
   return db!;
 }
+
+/**
+ * Util functions for Expo SQLite
+ */
+
+/**
+ * Executes SQL inside a transaction
+ * @param tx
+ * @param sql
+ * @param args
+ * @returns
+ */
+export function executeSql(
+  tx: SQLite.SQLTransaction,
+  sql: string,
+  args: Array<string | number | null>
+): Promise<SQLite.SQLResultSet> {
+  return new Promise((resolve, reject) => {
+    tx.executeSql(
+      sql,
+      args,
+      (_, res) => resolve(res),
+      (_, err) => {
+        reject(err);
+        return true;
+      }
+    );
+  });
+}
+
+/**
+ * Initiate a DB transaction
+ * @param db
+ * @param executor
+ * @returns
+ */
+export function transaction(
+  db: SQLite.WebSQLDatabase,
+  executor: SQLite.SQLTransactionCallback
+) {
+  return new Promise((resolve, reject) => {
+    db.transaction(executor, reject, () => resolve(undefined));
+  });
+}
