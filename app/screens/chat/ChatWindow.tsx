2de2f5565bab552ecb9f808c9bee03dbdac5f559
Snehanshu <hello@snehanshu.com>
2023-01-05 01:28:58 +0530

    Infinite scroll in chat window (with trpc and react query)

--- app/screens/chat/ChatWindow.tsx
+++ app/screens/chat/ChatWindow.tsx
@@ -1,34 +1,54 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useState } from "react";
 import { StyleSheet } from "react-native";
-import { List, TextInput, View } from "../../components/Themed";
+import { List, Text, TextInput, View } from "../../components/Themed";
 import { ChatsTabParamList } from "../../types";
 import { trpc } from "../../utils/trpc";
 import ChatMessage from "./ChatMessage";
 import { Ionicons } from "@expo/vector-icons";
 import { useMessages } from "../../utils/messages-repository";
+import { Message } from "../../../shared/types";
 
 export default function ChatWindowScreen({
   route: { params: groupInfo },
 }: NativeStackScreenProps<ChatsTabParamList, "ChatWindow">) {
   const messages = useMessages();
   const [messageText, setMessageText] = useState("");
-  const messagesQuery = trpc.school.messaging.fetchGroupMessages.useQuery({
-    groupIdentifier: groupInfo.id,
-  });
+  const messagesQuery =
+    trpc.school.messaging.fetchGroupMessages.useInfiniteQuery(
+      {
+        groupIdentifier: groupInfo.id,
+        limit: 20,
+      },
+      {
+        getNextPageParam: (lastPage) => lastPage.cursor,
+      }
+    );
   const utils = trpc.useContext();
 
   messages.useGroupMessageReceived(groupInfo.id, (message) => {
     utils.school.messaging.fetchGroupMessages.invalidate();
   });
 
+  // Combine messages from all pages
+  const allMessages: Message[] = [];
+  messagesQuery.data?.pages.forEach((page) => {
+    allMessages.push(...page.messages);
+  });
+
   return (
     <View style={styles.container}>
+      {messagesQuery.isFetchingNextPage ? (
+        <Text>Loading older messages...</Text>
+      ) : null}
+
       <List
         inverted
         style={styles.messages}
-        data={messagesQuery.data ?? []}
+        data={allMessages}
         renderItem={({ item }) => <ChatMessage message={item} />}
+        onEndReached={() => messagesQuery.fetchNextPage()}
+        onEndReachedThreshold={0.2}
       />
 
       <View style={styles.composer}>
