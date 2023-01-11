22e5be0d3d6d3ca06d225730dd1dccfbaee318c9
Snehanshu <hello@snehanshu.com>
2023-01-11 13:19:24 +0530

    Add greeting to homescreen

--- apps/app/screens/HomeTabScreen.tsx
+++ apps/app/screens/HomeTabScreen.tsx
@@ -1,11 +1,36 @@
 import { StyleSheet } from "react-native";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
+import { useCurrentUser } from "../utils/auth";
+
+/**
+ * Get a greeting by the time of day.
+ * Copied from https://github.com/elijahmanor/greeting-time/blob/master/index.js
+ * @param date
+ * @returns Greeting
+ */
+function greeting(date: Date): string {
+  const hour = date.getHours();
+  if (hour >= 5 && hour < 12) {
+    return "Good morning";
+  } else if (hour >= 12 && hour < 17) {
+    return "Good afternoon";
+  } else if ((hour >= 17 && hour <= 23) || hour < 5) {
+    return "Good evening";
+  }
+  return "Hello";
+}
 
 export default function HomeTabScreen({}: RootTabScreenProps<"HomeTab">) {
+  const { user } = useCurrentUser();
+
+  if (!user) return null;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>
+        {greeting(new Date())}, {user.name}!
+      </Text>
     </View>
   );
 }
