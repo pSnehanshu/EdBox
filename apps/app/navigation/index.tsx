e755015f57f35a4ff68937068e81960825ea7efa
simantaray <simantaray89@gmail.com>
2023-03-17 18:41:45 +0530

    icons

--- apps/app/navigation/index.tsx
+++ apps/app/navigation/index.tsx
@@ -1,4 +1,8 @@
-import { MaterialCommunityIcons, FontAwesome } from "@expo/vector-icons";
+import {
+  MaterialCommunityIcons,
+  FontAwesome,
+  Ionicons,
+} from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import {
   NavigationContainer,
@@ -217,9 +221,9 @@ function BottomTabNavigator() {
           title: "Home",
           headerTitle: school?.name ?? "Home",
           headerShown: true,
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons
-              name="home"
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "home-sharp" : "home-outline"}
               size={30}
               style={{ marginBottom: -3 }}
               color={color}
@@ -252,9 +256,9 @@ function BottomTabNavigator() {
         options={{
           title: "Chats",
           headerShown: true,
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons
-              name="chat"
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "ios-chatbox" : "chatbox-outline"}
               size={30}
               style={{ marginBottom: -3 }}
               color={color}
@@ -270,9 +274,9 @@ function BottomTabNavigator() {
             options={{
               title: "Routine",
               headerShown: true,
-              tabBarIcon: ({ color }) => (
-                <MaterialCommunityIcons
-                  name="timetable"
+              tabBarIcon: ({ focused, color }) => (
+                <Ionicons
+                  name={focused ? "md-timer-sharp" : "md-timer-outline"}
                   size={30}
                   style={{ marginBottom: -3 }}
                   color={color}
@@ -287,9 +291,9 @@ function BottomTabNavigator() {
             options={{
               title: "Exams",
               headerShown: true,
-              tabBarIcon: ({ color }) => (
-                <FontAwesome
-                  name="pencil-square-o"
+              tabBarIcon: ({ focused, color }) => (
+                <Ionicons
+                  name={focused ? "newspaper" : "newspaper-outline"}
                   size={30}
                   style={{ marginBottom: -3 }}
                   color={color}
@@ -306,9 +310,9 @@ function BottomTabNavigator() {
         options={{
           title: "Settings",
           headerShown: true,
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons
-              name="cog"
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons
+              name={focused ? "settings-sharp" : "settings-outline"}
               size={30}
               style={{ marginBottom: -3 }}
               color={color}
