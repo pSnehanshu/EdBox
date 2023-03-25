f504da7a8d34a9176bc99ae98cdb07ae19ad82d4
Snehanshu <hello@snehanshu.com>
2023-03-25 18:56:07 +0530

    Extract routine logic into a separate hook

--- apps/app/components/RoutineSlider.tsx
+++ apps/app/components/RoutineSlider.tsx
@@ -1,6 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import { format, isBefore, isWithinInterval } from "date-fns";
-import { useMemo } from "react";
+import { format } from "date-fns";
 import {
   Dimensions,
   Pressable,
@@ -16,8 +15,9 @@ import {
 } from "schooltalk-shared/misc";
 import type { DayOfWeek, TeacherRoutinePeriod } from "schooltalk-shared/types";
 import { useCurrentUser } from "../utils/auth";
+import { useRoutineWithGaps } from "../utils/routine-utils";
 import { trpc } from "../utils/trpc";
-import { GapPeriod, RoutinePeriod } from "../utils/types/routine-types";
+import { GapPeriod } from "../utils/types/routine-types";
 import { Text, View } from "./Themed";
 
 function getTimeFromHourMinute(hours: number, minutes: number) {
@@ -42,17 +42,18 @@ function SinglePeriodCard({ period }: SingleRoutineCardProps) {
 
   return (
     <View style={styles.container_carousel}>
-      {/* location and time for teacher */}
-      {isTeacher && !period.is_gap && (
+      {/* Class and time  */}
+      {!period.is_gap && (
         <View style={styles.time_loc}>
           <Text style={styles.time_dislay}>{format(start, "hh:mm aa")}</Text>
           <Text style={styles.time_dislay}>
-            {period.Class.name} {period.Section.name}
+            {period.Class.name} ({period.Section.name})
           </Text>
         </View>
       )}
+
       <Text style={styles.header}>
-        {!period.is_gap ? period?.Subject?.name : "Gap"}
+        {period.is_gap ? "Gap" : period?.Subject?.name}
       </Text>
 
       {isTeacher && !period.is_gap && (
@@ -92,81 +93,9 @@ export function RoutineSlider(props: RoutineSliderProps) {
           daysOfWeek: [dayOfWeek],
         });
 
-  // FIXME: Move this Logic into a utility: DRY
-  const { allPeriods, currentPeriodIndex } = useMemo(() => {
-    const allPeriods = routineQuery.data?.[dayOfWeek] ?? [];
-    type CustomPeriodType = (RoutinePeriod & { is_gap: false }) | GapPeriod;
-
-    // Sort be time
-    allPeriods.sort((p1, p2) => {
-      const p1start = getTimeFromHourMinute(p1.start_hour, p1.start_min);
-      const p2start = getTimeFromHourMinute(p2.start_hour, p2.start_min);
-
-      if (isBefore(p1start, p2start)) {
-        return -1;
-      } else if (isBefore(p2start, p1start)) {
-        return 1;
-      }
-      return 0;
-    });
-
-    const withGaps: CustomPeriodType[] = [];
-    allPeriods.forEach((p, i) => {
-      if (i === 0) {
-        withGaps.push({
-          ...p,
-          is_gap: false,
-        });
-      } else {
-        const previous = allPeriods[i - 1];
-        const hasGap =
-          previous.end_hour !== p.start_hour
-            ? true
-            : previous.end_min !== p.start_min;
-        if (hasGap) {
-          const gap: GapPeriod = {
-            is_gap: true,
-            start_hour: previous.end_hour,
-            start_min: previous.end_min,
-            end_hour: p.start_hour,
-            end_min: p.start_min,
-          };
-
-          withGaps.push(gap);
-        }
-
-        withGaps.push({
-          ...p,
-          is_gap: false,
-        });
-      }
-    });
-
-    // Determine the current period
-    const time = new Date();
-    const currentPeriodIndex = withGaps.findIndex((period) => {
-      const start = getTimeFromHourMinute(period.start_hour, period.start_min);
-      const end = getTimeFromHourMinute(period.end_hour, period.end_min);
-
-      return isWithinInterval(time, { start, end });
-    });
-
-    const firstPeriod = getTimeFromHourMinute(
-      withGaps[0]?.start_hour,
-      withGaps[0]?.start_min,
-    );
-
-    return {
-      allPeriods: withGaps,
-
-      currentPeriodIndex:
-        currentPeriodIndex < 0
-          ? isBefore(time, firstPeriod)
-            ? 0
-            : withGaps?.length - 1
-          : currentPeriodIndex,
-    };
-  }, [routineQuery.isFetching]);
+  const { allPeriods, currentPeriodIndex } = useRoutineWithGaps(
+    routineQuery.data?.[dayOfWeek] ?? [],
+  );
 
   if (routineQuery.isLoading) return <></>;
 
---
+++ apps/app/utils/routine-utils.ts
@@ -0,0 +1,113 @@
+import { isBefore, isWithinInterval } from "date-fns";
+import { useMemo } from "react";
+import type {
+  TeacherRoutinePeriod,
+  StudentRoutinePeriod,
+} from "schooltalk-shared/types";
+import type { GapPeriod } from "./types/routine-types";
+
+type Period = TeacherRoutinePeriod | StudentRoutinePeriod;
+type PeriodWithGapType = (Period & { is_gap: false }) | GapPeriod;
+
+/**
+ * Returns date object representing today with given hour and minute
+ * @param hours
+ * @param minutes
+ * @returns
+ */
+// TODO: memoize the above function
+function getTimeFromHourMinute(hours: number, minutes: number) {
+  const startTime = new Date();
+  startTime.setHours(hours);
+  startTime.setMinutes(minutes);
+  startTime.setSeconds(0);
+  startTime.setMilliseconds(0);
+
+  return startTime;
+}
+
+export function useRoutineWithGaps(periods: Period[]) {
+  const { allPeriods, currentPeriodIndex } = useMemo(() => {
+    const allPeriods = periods.slice();
+
+    // Sort be time
+    allPeriods.sort((p1, p2) => {
+      const p1start = getTimeFromHourMinute(p1.start_hour, p1.start_min);
+      const p2start = getTimeFromHourMinute(p2.start_hour, p2.start_min);
+
+      if (isBefore(p1start, p2start)) {
+        return -1;
+      } else if (isBefore(p2start, p1start)) {
+        return 1;
+      }
+      return 0;
+    });
+
+    const periodsWithGaps: PeriodWithGapType[] = [];
+    allPeriods.forEach((p, i) => {
+      // Can't be gap at the beginning
+      if (i === 0) {
+        periodsWithGaps.push({
+          ...p,
+          is_gap: false,
+        });
+      } else {
+        const previous = allPeriods[i - 1];
+
+        // Check if there's any gap between the last period and this period
+        const hasGap =
+          previous.end_hour !== p.start_hour
+            ? true
+            : previous.end_min !== p.start_min;
+
+        if (hasGap) {
+          // There's a gap, push it
+          const gap: GapPeriod = {
+            is_gap: true,
+            start_hour: previous.end_hour,
+            start_min: previous.end_min,
+            end_hour: p.start_hour,
+            end_min: p.start_min,
+          };
+
+          periodsWithGaps.push(gap);
+        }
+
+        // Now push the period
+        periodsWithGaps.push({
+          ...p,
+          is_gap: false,
+        });
+      }
+    });
+
+    // Determine the current period
+    const time = new Date();
+    const currentPeriodIndex = periodsWithGaps.findIndex((period) => {
+      const start = getTimeFromHourMinute(period.start_hour, period.start_min);
+      const end = getTimeFromHourMinute(period.end_hour, period.end_min);
+
+      return isWithinInterval(time, { start, end });
+    });
+
+    const firstPeriodTime = getTimeFromHourMinute(
+      periodsWithGaps[0]?.start_hour,
+      periodsWithGaps[0]?.start_min,
+    );
+
+    return {
+      allPeriods: periodsWithGaps,
+      currentPeriodIndex:
+        currentPeriodIndex < 0
+          ? isBefore(time, firstPeriodTime)
+            ? 0
+            : periodsWithGaps?.length - 1
+          : currentPeriodIndex,
+    };
+  }, [periods]);
+
+  return {
+    allPeriods,
+    currentPeriodIndex,
+  };
+}
