7644c44f71128d9dde8c6a9f7384b40b429d9ec1
Snehanshu <hello@snehanshu.com>
2023-03-26 00:07:41 +0530

    Empty out AboutAppScreen

--- apps/app/screens/settings/AboutAppScreen.tsx
+++ apps/app/screens/settings/AboutAppScreen.tsx
@@ -1,24 +1,28 @@
-import { Button } from "react-native";
-import * as Updates from "expo-updates";
-import { View } from "../../components/Themed";
+import { Button, ScrollView } from "react-native";
+// import * as Updates from "expo-updates";
+// import { Text } from "../../components/Themed";
 
 export default function AboutAppScreen() {
-  async function onFetchUpdateAsync() {
-    try {
-      const update = await Updates.checkForUpdateAsync();
+  // async function onFetchUpdateAsync() {
+  //   try {
+  //     const update = await Updates.checkForUpdateAsync();
 
-      if (update.isAvailable) {
-        await Updates.fetchUpdateAsync();
-        await Updates.reloadAsync();
-      }
-    } catch (error) {
-      alert(`Error fetching latest Expo update: ${error}`);
-    }
-  }
+  //     if (update.isAvailable) {
+  //       await Updates.fetchUpdateAsync();
+  //       await Updates.reloadAsync();
+  //     }
+  //   } catch (error) {
+  //     alert(`Error fetching latest Expo update: ${error}`);
+  //   }
+  // }
 
   return (
-    <View>
-      <Button title="Fetch update" onPress={onFetchUpdateAsync} />
-    </View>
+    <ScrollView>
+      {/* <Button title="Fetch update" onPress={onFetchUpdateAsync} />
+      <Text>
+        isEmbeddedLaunch: {Updates.isEmbeddedLaunch ? "true" : "false"}
+      </Text>
+      <Text>isEmbeddedLaunch: {JSON.stringify(Updates.manifest, null, 2)}</Text> */}
+    </ScrollView>
   );
 }
