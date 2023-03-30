009732233401fe05ba9138ca21e330d17881ee56
Snehanshu <hello@snehanshu.com>
2023-03-31 00:22:04 +0530

    Menu sheet improvements

--- apps/app/screens/MenuScreen.tsx
+++ apps/app/screens/MenuScreen.tsx
@@ -58,80 +58,65 @@ export default function MenuScreen() {
   const refRBSheet = useRef<RBSheet>(null);
 
   return (
-    <>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: 1,
+    <View style={styles.container}>
+      <MaterialCommunityIcons
+        name="dots-grid"
+        size={34}
+        onPress={() => refRBSheet?.current?.open()}
+      />
+      <Text style={styles.tab_icon_text}>Menu</Text>
+
+      <RBSheet
+        ref={refRBSheet}
+        closeOnDragDown={true}
+        closeOnPressMask={true}
+        height={150}
+        customStyles={{
+          container: styles.bottom_sheet_container,
+          wrapper: styles.bottom_sheet_wrapper,
+          draggableIcon: styles.bottom_sheet_draggable_icon,
         }}
       >
-        <MaterialCommunityIcons
-          name="dots-grid"
-          size={34}
-          onPress={() => refRBSheet?.current?.open()}
-        />
-        <Text style={{ fontSize: 10, marginTop: 0 }}>Menu</Text>
-        <RBSheet
-          ref={refRBSheet}
-          closeOnDragDown={true}
-          closeOnPressMask={false}
-          height={150}
-          customStyles={{
-            container: {
-              borderTopRightRadius: 15,
-              borderTopLeftRadius: 15,
-              shadowColor: "#000",
-              shadowOffset: {
-                width: 0,
-                height: 5,
-              },
-              shadowOpacity: 0.34,
-              shadowRadius: 6.27,
-              elevation: 10,
-            },
-            wrapper: {
-              backgroundColor: "transparent",
-            },
-            draggableIcon: {
-              backgroundColor: "#000",
-            },
-          }}
-        >
-          <View style={styles.container}>
-            {MenuItems.map((item, i) => (
-              <Pressable
-                key={i}
-                onPress={item.onPress}
-                style={({ pressed }) => [
-                  styles.item,
-                  { opacity: pressed ? 0.5 : 1 },
-                ]}
-              >
-                <View style={styles.icon}>{item.icon}</View>
-                <View style={styles.titleArea}>
-                  <Text style={styles.itemTitle}>{item.name}</Text>
-                </View>
-              </Pressable>
-            ))}
-          </View>
-        </RBSheet>
-      </View>
-    </>
+        <View style={styles.list_container}>
+          {MenuItems.map((item, i) => (
+            <Pressable
+              key={i}
+              onPress={() => {
+                refRBSheet.current?.close();
+                item.onPress?.();
+              }}
+              style={({ pressed }) => [
+                styles.item,
+                { opacity: pressed ? 0.5 : 1 },
+              ]}
+            >
+              <View style={styles.icon}>{item.icon}</View>
+              <View style={styles.titleArea}>
+                <Text style={styles.itemTitle}>{item.name}</Text>
+              </View>
+            </Pressable>
+          ))}
+        </View>
+      </RBSheet>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: "100%",
-    // backgroundColor: "red",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 1,
   },
+  list_container: {
+    height: "100%",
+  },
+  tab_icon_text: { fontSize: 10, marginTop: 0 },
   item: {
     paddingVertical: 16,
     borderBottomColor: "gray",
     borderBottomWidth: 0.5,
-    // flex: 1,
     flexDirection: "row",
     paddingLeft: 8,
   },
@@ -146,5 +131,23 @@ const styles = StyleSheet.create({
   },
   itemTitle: {
     fontSize: 16,
+  },
+  bottom_sheet_container: {
+    borderTopRightRadius: 15,
+    borderTopLeftRadius: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10,
+  },
+  bottom_sheet_wrapper: {
+    backgroundColor: "transparent",
+  },
+  bottom_sheet_draggable_icon: {
+    backgroundColor: "#000",
   },
 });
