f1a435335acd58e26a9a1188f28096b9d8ed7746
Snehanshu <hello@snehanshu.com>
2023-03-31 12:36:05 +0530

    Use zod to validate env vars

--- apps/backend/config.ts
+++ apps/backend/config.ts
@@ -1,5 +1,15 @@
-const CONFIG = {
-  otpLength: 6,
-};
+import { z } from "zod";
 
-export default CONFIG;
+const ConfigSchema = z.object({
+  OTP_LENGTH: z.number().int().default(6),
+  S3_UPLOAD_URL_EXPIRY_SECONDS: z
+    .number()
+    .int()
+    .default(30 * 60),
+  S3_USERCONTENT_BUCKET: z.string(),
+  NODE_ENV: z
+    .enum(["production", "development", "staging"])
+    .default("development"),
+});
+
+export default ConfigSchema.parse(process.env);
