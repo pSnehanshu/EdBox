17c32823593fc6354d6e5c9b29fa08d1015758d4
simantaray <simantaray0@gmail.com>
2023-04-18 22:44:05 +0530

    create test modal +

---
+++ apps/app/screens/exam/TestModal.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { Dialog, ListItem } from "@rneui/themed";
+import React from "react";
+import { TextInput } from "../../components/Themed";
+import ModalSelector from "react-native-modal-selector";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import useColorScheme from "../../utils/useColorScheme";
+
+export default function () {
+  const scheme = useColorScheme();
+  const iconColor = scheme === "dark" ? "white" : "black";
+  const ChevronIcon = (
+    <MaterialCommunityIcons name="chevron-right" color={iconColor} size={16} />
+  );
+  return (
+    <Dialog
+      isVisible={true}
+      //   onBackdropPress={props.onClose}
+      animationType="fade"
+      style={{ width: "100%" }}
+    >
+      <Dialog.Title title={"Create Test"} />
+
+      <ModalSelector
+        data={[]}
+        //   onChange={(item) => setSelectedSubject(item.key)}
+        animationType="fade"
+        //   selectedKey={selectedSubject}
+      >
+        <ListItem>
+          <ListItem.Content>
+            <ListItem.Title>Subject</ListItem.Title>
+            <ListItem.Subtitle>{"Select subject"}</ListItem.Subtitle>
+          </ListItem.Content>
+          {ChevronIcon}
+        </ListItem>
+      </ModalSelector>
+
+      <Dialog.Actions>
+        <Dialog.Button
+          title="Done"
+          onPress={() => {
+            // props.onChange?.(value);
+            // props.onClose?.();
+          }}
+          type="solid"
+        />
+      </Dialog.Actions>
+    </Dialog>
+  );
+}
