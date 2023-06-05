4eacb90a6f5ef361f55d8d45dd0280a990839756
Snehanshu <hello@snehanshu.com>
2023-06-05 15:45:08 +0530

    Role selector uses CustomSelect now on

--- apps/app/components/StaticRoleSelector.tsx
+++ apps/app/components/StaticRoleSelector.tsx
@@ -1,20 +1,13 @@
-import { MaterialIcons } from "@expo/vector-icons";
-import { ListItem } from "@rneui/themed";
 import { useEffect, useMemo } from "react";
-import { Pressable } from "react-native";
 import { getUserRoleHierarchical, StaticRole } from "schooltalk-shared/misc";
 import { useCurrentUser } from "../utils/auth";
 import { useConfig, useConfigUpdate } from "../utils/config";
-import useColorScheme from "../utils/useColorScheme";
-import { ScrollView, View } from "./Themed";
+import { CustomSelect, type SelectControlParams } from "./CustomSelect";
 
 interface StaticRoleSelector {
-  onChange?: (role: StaticRole) => void;
+  children?: (params: SelectControlParams) => React.ReactNode;
 }
-export function StaticRoleSelector({ onChange }: StaticRoleSelector) {
-  const scheme = useColorScheme();
-  const color = scheme === "dark" ? "white" : "black";
-
+export function StaticRoleSelector({ children }: StaticRoleSelector) {
   const { isLoggedIn, user } = useCurrentUser();
   const config = useConfig();
   const setConfig = useConfigUpdate();
@@ -41,41 +34,17 @@ export function StaticRoleSelector({ onChange }: StaticRoleSelector) {
   }, [config.activeStaticRole, user, isLoggedIn]);
 
   return (
-    <View>
-      <ScrollView>
-        {availableRoles.map((role) => (
-          <Pressable
-            key={role}
-            onPress={() => {
-              setConfig({ activeStaticRole: role });
-              onChange?.(role);
-            }}
-            style={({ pressed }) => ({ opacity: pressed ? 0.5 : 1 })}
-          >
-            <ListItem>
-              <ListItem.Content>
-                <ListItem.Title>
-                  {StaticRole[role].split("_").join(" ").toUpperCase()}
-                </ListItem.Title>
-              </ListItem.Content>
-
-              {config.activeStaticRole === role ? (
-                <MaterialIcons
-                  name="radio-button-checked"
-                  size={24}
-                  color={color}
-                />
-              ) : (
-                <MaterialIcons
-                  name="radio-button-unchecked"
-                  size={24}
-                  color={color}
-                />
-              )}
-            </ListItem>
-          </Pressable>
-        ))}
-      </ScrollView>
-    </View>
+    <CustomSelect
+      isSingle
+      selected={config.activeStaticRole}
+      items={availableRoles}
+      title="Select your role"
+      idExtractor={(item) => item}
+      labelExtractor={(item) =>
+        StaticRole[item].split("_").join(" ").toUpperCase()
+      }
+      onSubmit={(item) => setConfig({ activeStaticRole: item })}
+      control={children}
+    />
   );
 }
