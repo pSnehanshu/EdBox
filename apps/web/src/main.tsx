f55ffba1d17f44a0eecb78829411e11086939406
Snehanshu Phukon <hello@snehanshu.com>
2023-06-26 16:15:57 +0530

    Setup trpc client in web

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
 import App from "./App.tsx";
+import { trpc } from "./utils/trpc";
 import "./index.css";
 
+const ProvidersTree: React.FC = () => {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: "http://localhost:3000/trpc",
+          async headers() {
+            return {
+              // authorization: getAuthCookie(),
+            };
+          },
+        }),
+      ],
+    }),
+  );
+
+  return (
+    <React.StrictMode>
+      <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </trpc.Provider>
+    </React.StrictMode>
+  );
+};
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ProvidersTree />,
 );
