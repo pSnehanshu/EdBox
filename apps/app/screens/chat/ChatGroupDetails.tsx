bf42a0d8a5cd5a18db44f90554c085703c4f894d
simantar <simantaray89@gmail.com>
2023-07-11 21:57:54 +0530

    media slider

--- apps/app/screens/chat/ChatGroupDetails.tsx
+++ apps/app/screens/chat/ChatGroupDetails.tsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet } from "react-native";
+import { Dimensions, Pressable, StyleSheet } from "react-native";
 import { ScrollView, Text, View } from "../../components/Themed";
 import type { RootStackScreenProps } from "../../utils/types/common";
 import { Image, ListItem } from "@rneui/themed";
@@ -6,6 +6,7 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import useColorScheme from "../../utils/useColorScheme";
 import { ModalTextInput } from "../../components/ModalTextInput";
 import { useState } from "react";
+import { Carousel } from "react-native-snap-carousel";
 
 export default function ChatGroupDetails({
   navigation,
@@ -18,6 +19,11 @@ export default function ChatGroupDetails({
   const [groupName, setGroupName] = useState<string | undefined>(
     params.name ?? undefined,
   );
+  const [groupDetails, setGroupDetails] = useState<string | undefined>(
+    "chat details...." ?? undefined,
+  );
+  const SLIDER_WIDTH = Dimensions.get("window").width;
+  const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.35);
 
   return (
     <View style={{ height: "100%" }}>
@@ -55,9 +61,53 @@ export default function ChatGroupDetails({
               defaultValue={groupName}
               title="Group Name"
             />
+            <ModalTextInput
+              onChange={setGroupDetails}
+              defaultValue={groupDetails}
+              title="Group Details"
+            />
+            <View style={styles.media_container}>
+              <Text style={{ marginLeft: 16, fontSize: 16 }}>
+                Media, links and docs
+              </Text>
+              <Carousel
+                layout="default"
+                vertical={false}
+                layoutCardOffset={9}
+                firstItem={1}
+                data={[
+                  { name: "1" },
+                  { name: "2" },
+                  { name: "1" },
+                  { name: "2" },
+                  { name: "1" },
+                  { name: "2" },
+                ]}
+                renderItem={({ item, index }) => (
+                  <SingleMediaCard period={item} index={index} />
+                )}
+                sliderWidth={SLIDER_WIDTH}
+                itemWidth={ITEM_WIDTH}
+                inactiveSlideShift={0}
+                // useScrollView={true}
+                inactiveSlideOpacity={1}
+                inactiveSlideScale={1}
+                activeSlideAlignment="start"
+                useExperimentalSnap
+                slideStyle={{ marginLeft: 16 }}
+              />
+            </View>
           </>
         )}
       </ScrollView>
+    </View>
+  );
+}
+
+export function SingleMediaCard({ details }: any) {
+  return (
+    <View style={styles.container_carousel}>
+      <Text> </Text>
     </View>
   );
 }
@@ -100,5 +150,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderRadius: 100,
     padding: 4,
+  },
+  media_container: {
+    flexDirection: "column",
+    paddingTop: 16,
+    justifyContent: "center",
+  },
+  container_carousel: {
+    alignItems: "center",
+    backgroundColor: "gray",
+    borderRadius: 8,
+    margin: 4,
+    height: 120,
   },
 });
