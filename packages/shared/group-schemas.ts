5973ff3a9d33967ac424674e0cb65e87f524acb3
Snehanshu <hello@snehanshu.com>
2023-07-15 10:14:15 +0530

    Added group activity payload schemas

---
+++ packages/shared/group-schemas.ts
@@ -0,0 +1,66 @@
+import { z } from "zod";
+import { GroupActivityType } from "@prisma/client";
+
+const MessageNewSchema = z.object({
+  t: z.literal(GroupActivityType.message_new),
+  body: z.string(),
+  fileIds: z.string().cuid().array().optional(),
+});
+
+const MessageEditSchema = z.object({
+  t: z.literal(GroupActivityType.message_edit),
+  body: z.string(),
+  fileIds: z.string().cuid().array().optional(),
+});
+
+const MessageDeleteSchema = z.object({
+  t: z.literal(GroupActivityType.message_delete),
+});
+
+const MemberAddSchema = z.object({
+  t: z.literal(GroupActivityType.member_add),
+  id: z.string().cuid(),
+  name: z.string(),
+});
+
+const MemberRemoveSchema = z.object({
+  t: z.literal(GroupActivityType.member_remove),
+  name: z.string(),
+  id: z.string().cuid(),
+});
+
+const NameChangeSchema = z.object({
+  t: z.literal(GroupActivityType.name_change),
+  name: z.string(),
+});
+
+const DescriptionChangeSchema = z.object({
+  t: z.literal(GroupActivityType.description_change),
+  des: z.string(),
+});
+
+const IconChangeSchema = z.object({
+  t: z.literal(GroupActivityType.icon_change),
+  fileId: z.string().cuid(),
+});
+
+const ReadReceiptSchema = z.object({
+  t: z.literal(GroupActivityType.read),
+});
+
+const DeliveryReceiptSchema = z.object({
+  t: z.literal(GroupActivityType.delivered),
+});
+
+export const ActivityPayloadSchema = z.discriminatedUnion("t", [
+  MessageNewSchema,
+  MessageEditSchema,
+  MessageDeleteSchema,
+  MemberAddSchema,
+  MemberRemoveSchema,
+  NameChangeSchema,
+  DescriptionChangeSchema,
+  IconChangeSchema,
+  ReadReceiptSchema,
+  DeliveryReceiptSchema,
+]);
