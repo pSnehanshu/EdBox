e537aa1ef3157653f8b30a706cfbf5b473485523
Snehanshu <hello@snehanshu.com>
2023-08-05 16:19:49 +0530

    Add a nice spinner

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,17 @@
-import React, { Suspense, useMemo, useState } from "react";
+import React, { Suspense, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Flex, ChakraProvider, CircularProgress } from "@chakra-ui/react";
 import Routes from "./routes.tsx";
 import { trpc, links } from "./utils/trpc";
+
+function Loading() {
+  return (
+    <Flex h="100vh" justifyContent="center" alignItems="center">
+      <CircularProgress isIndeterminate size="120px" />
+    </Flex>
+  );
+}
 
 const ProvidersTree: React.FC = () => {
   const [queryClient] = useState(
@@ -26,7 +34,7 @@ const ProvidersTree: React.FC = () => {
       <ChakraProvider>
         <trpc.Provider client={trpcClient} queryClient={queryClient}>
           <QueryClientProvider client={queryClient}>
-            <Suspense fallback={<h1>Please wait...</h1>}>
+            <Suspense fallback={<Loading />}>
               <Routes />
             </Suspense>
           </QueryClientProvider>
