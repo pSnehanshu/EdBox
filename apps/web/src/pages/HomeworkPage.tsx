94b19b29449b534fd2cab98500af73d1e89f5eb6
simantar <simantaray89@gmail.com>
2023-08-18 21:18:39 +0530

    homework data

--- apps/web/src/pages/HomeworkPage.tsx
+++ apps/web/src/pages/HomeworkPage.tsx
@@ -1,8 +1,20 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Stack,
+  StackDivider,
+  Text,
+} from "@chakra-ui/react";
 import { StaticRole } from "schooltalk-shared/misc";
 import { useConfig } from "../utils/atoms";
 import { trpc } from "../utils/trpc";
 import { Homework } from "schooltalk-shared/types";
+import { useMemo } from "react";
+import { format, isPast, parseISO } from "date-fns";
+import { MdOutlineAttachFile } from "react-icons/md";
 
 const pageLimit = 10;
 
@@ -51,7 +63,69 @@ export default function HomeworkPage() {
 
   return (
     <Box ml={{ base: 0, md: "72" }} p="4">
-      <h1>Homework</h1>
+      <Card>
+        <CardHeader>
+          <Heading size="md">Home Works</Heading>
+        </CardHeader>
+        <CardBody>
+          <Stack divider={<StackDivider />} spacing="4">
+            <Box>
+              {homeworks &&
+                homeworks.map((item, index) => (
+                  <SingleHomework key={index} homework={item} />
+                ))}
+            </Box>
+          </Stack>
+        </CardBody>
+      </Card>
     </Box>
+  );
+}
+
+interface homeworkProps {
+  homework: Homework;
+}
+
+function SingleHomework({ homework }: homeworkProps) {
+  const dueDate = useMemo(
+    () => (homework.due_date ? parseISO(homework.due_date) : null),
+    [homework.due_date],
+  );
+
+  const dueDateStr = useMemo(() => {
+    if (!dueDate) return null;
+
+    return `${format(dueDate, "MMM dd, yyyy")}\n${format(
+      dueDate,
+      "hh:mm aaa",
+    )}`;
+  }, [dueDate]);
+
+  const isPastDue = useMemo(
+    () => (dueDate ? isPast(dueDate) : false),
+    [dueDate],
+  );
+  const classAndSection = useMemo(
+    () =>
+      `Class ${homework.Class.name ?? homework.Class.numeric_id} (${
+        homework.Section.name ?? homework.Section.numeric_id
+      })`,
+    [homework.Class.name, homework.Section.name],
+  );
+
+  return (
+    <>
+      <Heading size="xs" textTransform="uppercase">
+        {homework.Subject.name} — {classAndSection}
+      </Heading>
+      <Text pt="2" fontSize="sm">
+        {homework.text ? homework.text : ""}
+        <MdOutlineAttachFile />
+        {homework.Attachments.length}
+      </Text>
+      <Text pt="2" fontSize="sm">
+        Due date: {"\n" + dueDateStr}
+      </Text>
+    </>
   );
 }
